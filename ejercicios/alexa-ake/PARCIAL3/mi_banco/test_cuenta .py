9654d78f00a2494864df23e53f978a46e5a6bb36
Alexa Ake <alexa.ake_pm25070@ceyte.edu.mx>
2026-05-13 00:45:28 -0600

    Se agrega PARCIAL3 completo con todos los ejercicios

--- ejercicios/alexa-ake/PARCIAL3/mi_banco/test_cuenta .py
+++ ejercicios/alexa-ake/PARCIAL3/mi_banco/test_cuenta .py
@@ -1,31 +1,33 @@
 import unittest
 from cuenta import Cuenta
 
-class TestCuenta(unittest.testCase):
+class TestCuenta(unittest.TestCase):
 
     def setUp(self):
-        self.cuenta = cuenta("Fulanito Perez Mengano", "001")
+        """
+        Se ejecuta antes de cada prueba.
+        """
+        self.cuenta = Cuenta("Fulanito Perez Mengano", "001")
 
-# -----------------PRUEBAS DEL CONSTRUCTOR-----------------
+    #------------ Pruebas del Constructor ------------
 
-def test_validar_saldo(self):
-    self.assertEqual(self.cuenta.saldo,0,"El saldo inicial deberia ser 0 por defecto")
+    def test_validad_saldo(self):
+        self.assertEqual(self.cuenta.saldo, 0, "El saldo inicial debe ser 0 por defecto")
 
-def test_validar_clientes(self):
-    self.assertEqual(self.cuenta.cliente, "Fulanito Perez Mengano" , "El nombre del cliente no es correcto")
+    def test_valida_cliente(self):
+        self.assertEqual(self.cuenta.Cliente, "Fulanito Perez Mengano", "El nombre del cliente no es correcto")
 
+    #------------ Pruebas de Depósito ------------
 
-#-----------------PRUEBAS DE DEPOSITO-----------------------
+    def test_depositar_dinero_valido(self):
+        result = self.cuenta.deposito(500.00)
+        self.assertTrue(result)
+        self.assertEqual(self.cuenta.saldo, 500, "El saldo actual debería ser de 500.00")
 
-def test_depositar_dinero_valido(self):
-    result = self.cuenta.deposito(500)
-    self.assertTrue(result)
-    self.assertEqual(self.cuenta.saldo, 500, "El saldo actuar deberia ser de 500.00")
-
-    def test_depositar_cantidad_negativa(self):
-        result = self.cuenta.deposito(-200)
+    def test_depositar_dinero_negativa(self):
+        result = self.cuenta.deposito(-200.00)
         self.assertFalse(result)
-        self.assertEqual(self.cuenta.saldo,0, "El saldo actual deberia ser 0")
+        self.assertEqual(self.cuenta.saldo, 0, "El saldo actual debería ser de 0")
 
     #test para validar deposito en 0
     def test_depositar_dinero_cero(self):
@@ -58,11 +60,10 @@ def test_depositar_dinero_valido(self):
         result = self.cuenta.retiro(0)
         self.assertFalse(result)
         self.assertEqual(self.cuenta.saldo, 300, "El saldo actual debería ser de 300.00")
+if _name_ == "_main_":
+    unittest.main()
 
-    if __name__== "_main_":
-        unittest.main()
-
-    #test para validar cantidad mayor la saldo
+    #test para validar cantidad mayor al saldo
     def test_retirar_dinero_mayor_saldo(self):
         self.cuenta.deposito(300.00)
         result = self.cuenta.retiro(400.00)
